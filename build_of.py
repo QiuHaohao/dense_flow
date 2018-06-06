e6995e88fdde18fdfd0fab527a3cf4e1e38b26fd
Qiu Haohao <qiuh0008@e.ntu.edu.sg>
2018-06-06 15:42:27 +0800

    Make build_of.py preserve directory structure

--- build_of.py
+++ build_of.py
@@ -36,13 +36,14 @@ def dump_frames(vid_path):
 def run_optical_flow(vid_item, dev_id=0):
     vid_path = vid_item[0]
     vid_id = vid_item[1]
+    user_id = vid_path.split("/")[-2]
     vid_name = vid_path.split('/')[-1].split('.')[0]
-    out_full_path = os.path.join(out_path, vid_name)
+    out_full_path = os.path.join(out_path, user_id, vid_name)
     if os.path.exists(out_full_path):
         print("{} {} already done, skipping".format(vid_id, vid_name))
         return True
     try:
-        os.mkdir(out_full_path)
+        os.makedirs(out_full_path)
     except OSError:
         pass
 
@@ -64,13 +65,14 @@ def run_optical_flow(vid_item, dev_id=0):
 def run_warp_optical_flow(vid_item, dev_id=0):
     vid_path = vid_item[0]
     vid_id = vid_item[1]
+    user_id = vid_path.split('/')[-2]
     vid_name = vid_path.split('/')[-1].split('.')[0]
-    out_full_path = os.path.join(out_path, vid_name)
+    out_full_path = os.path.join(out_path, user_id, vid_name)
     if os.path.exists(out_full_path):
         print("{} {} already done, skipping".format(vid_id, vid_name))
         return True
     try:
-        os.mkdir(out_full_path)
+        os.makedirs(out_full_path)
     except OSError:
         pass
 
